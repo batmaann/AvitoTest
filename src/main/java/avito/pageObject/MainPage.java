9c4fae6cdb1a1fac59f31ebf2727868fd10f388a
fox <staslive2014@gmail.com>
2025-04-24 20:53:35 +0300

    v2.0 ебучий кусок говна работает, ебать я ещё что-то пишу и делаю здесь, мне заебало что я на работе не мог в комитах материться а тут могу идите нахуй если не нравиться!

--- src/main/java/avito/pageObject/MainPage.java
+++ src/main/java/avito/pageObject/MainPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 
+import java.util.function.Consumer;
+
 public class MainPage {
     public MainPage(WebDriver webDriver) {
         this.webDriver = webDriver;
@@ -13,13 +15,15 @@ public class MainPage {
     //локаторы
     private By categoriesButto = By.className("top-rubricator-newWrapper-VLDwY");
     private By categoriesButton = By.xpath("//*[@class=\"top-rubricator-buttonText-c0xFa\"]");
-
-
-
     private By chocieButton = By.xpath("//*[@data-marker=\"location/tooltip-agree\"]");
-
-
     private By realtyСategoryButton = By.xpath("//div[5][@class='new-rubricator-content-rootCategory-S2VPI']");
+    private By beforeButton = By.xpath("//button[span/span[text()='Наконец-то']]");
+    private By goodButton = By.xpath("//button[span/span[text()='Хорошо']]");
+    private By yesButton = By.xpath("//button[span[text()='Да']]");
+
+
+
+
 
 
 
@@ -41,7 +45,37 @@ public class MainPage {
     }
     public void choiceCategoryMove() {
         webDriver.findElement(realtyСategoryButton).click();
+    }
 
+    //кусов говвна
+    public void clickElement(By elementLocator) {
+        webDriver.findElement(elementLocator).click();
+    }
+
+
+
+    public void actionStepBefore() {
+        try {
+            clickElement(beforeButton);
+        } catch (Exception e) {
+            System.out.println("[WARN] Не удалось кликнуть beforeButton: " + e.getMessage());
+        }
+
+        // Клик на goodButton (если не получится - пропускаем)
+        try {
+            clickElement(goodButton);
+        } catch (Exception e) {
+            System.out.println("[WARN] Не удалось кликнуть goodButton: " + e.getMessage());
+        }
+        try {
+            clickElement(yesButton);
+        } catch (Exception e) {
+            System.out.println("[WARN] Не удалось кликнуть yesButton: " + e.getMessage());
+        }
+    }
+
+    public void withLocator(Consumer<By> action) {
+        action.accept(beforeButton); // Передаём приватный локатор в лямбду
     }
 
 
--- src/test/java/avito_test/CategoryTest.java
+++ src/test/java/avito_test/CategoryTest.java
@@ -6,17 +6,21 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Sleeper;
+
 
 import java.io.File;
 import java.time.Duration;
 
 import static avito.appConfig.AppConfig.base_URL;
+import static java.lang.Thread.sleep;
 
 public class CategoryTest {
     WebDriver driver = new FirefoxDriver();
@@ -28,21 +32,26 @@ public class CategoryTest {
         System.setProperty("webdriver.gecko.driver", "/Users/prime/MK/Education/AvitoTest/drivers/geckodriver");
         driver.get(base_URL);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        try {
-            mainPage.clickСityButton();
-        } catch (NoSuchElementException e) {
-            // Если кнопки нет, ловим исключение, но продолжаем выполнение теста
-        }
+
+
+        mainPage.actionStepBefore();
     }
 
     @Test
     @DisplayName("Проверка категория")
     @Description("null")
     public void choiceCategory() {
-        mainPage.clickСategoriesButton();
-        //for (int i = 2; i < 5; i++) {
-            mainPage.choiceCategoryMove();
-        //}
+
+        mainPage.actionStepBefore();
+
+        Sleeper sleeper = Sleeper.SYSTEM_SLEEPER;
+        try {
+            sleeper.sleep(Duration.ofSeconds(21)); // Работает в Selenium 4.3+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Sleep interrupted", e);
+        }
+
     }
 
     @After
